7ee867f2351cb04fd0fbb676429554bf2a0c0239
designerasun <asuntheawesome@gmail.com>
2022-03-03 12:19:27 +0900

    feat : add swift playground

---
+++ swift/playground/hello.swift
@@ -0,0 +1,19 @@
+import Foundation
+
+// variable declaration
+var myString = "Hello Swift"
+var one = 1
+var two = 2
+
+two = 22
+
+let z = 3 // let is for an immutable variable
+// z = 4 // error
+
+class Person {
+    static let jake = Person()
+    let lastName = "Sung"
+}
+
+var Jake = Person()
+let myLastName = Jake.lastName // "Sung"
